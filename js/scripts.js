e37600a3bc816c64661213a001c94bca14096772
Jessica Sheridan and Kheifer Fuller <jessica@>
2017-07-19 14:02:11 -0700

    include step to add additional player.

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,23 @@ function Player (user) {
   this.rolls = [];
   this.turnTotal = 0;
   this.gameTotal = 0;
-  this.turnOver = 0;
+  this.turn = 1;
 }
 
 Player.prototype.diceRoller =function() {
   return Math.floor(Math.random() * 6) + 1;
+}
+
+//return turnTotal? to add to gameTotal?
+Player.prototype.takeTurn = function(roll){
+  this.rolls.push(roll);
+  if(roll=== 1){
+    this.turn = 0;
+    return 0;
+  }else {
+    return getSum(this.rolls);
+    //$("#turnTotal").text(getSum(player1.rolls));
+  }
 }
 
 function getSum(numbers){
@@ -20,23 +32,22 @@ function getSum(numbers){
 
 $(document).ready(function() {
   var hold = 0;
-  $("#roll1").click(function() {
-    var player1 = new Player ("1");
-    var roll = player1.diceRoller();
-    player1.rolls.push(roll);
-    $(".die1").text(roll);
+  var player1 = new Player ("1");
+  var player2 = new Player ("2");
 
-    if(roll=== 1){
-      player1.turnOver = 1;
-      alert("Turn Over: 1 rolled")
-      $("#turnTotal").text("0");
-    }else{
-      $("#turnTotal").text(getSum(player1.rolls));
+
+
+  $("#roll1").click(function() {
+    var roll = player1.diceRoller();
+    if (player1.turn) {
+      $(".die1").text(roll);
+      player1.turnTotal = player1.takeTurn(roll);
+      $("#turnTotal").text(player1.turnTotal);
     }
-});
+  });
   $("#hold1").click(function() {
     hold = 1;
-    alert("hold event: turn over")
+    alert("hold event: turn over");
 });
 
 });
