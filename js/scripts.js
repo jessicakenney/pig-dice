e348bd60dba0c31076443eacf8463860c80f0833
Jessica Sheridan and Kheifer Fuller <kheifer@>
2017-07-19 10:07:48 -0700

    include specification to add 2 consecutive rolls.

--- js/scripts.js
+++ js/scripts.js
@@ -4,13 +4,24 @@ function diceRoller() {
 function isOne(number){
   return number === 1;
 }
-
+function getSum(numbers){
+  var sum = 0;
+  numbers.forEach(function(number) {
+    sum += number;
+  });
+  return sum;
+}
 
 $(document).ready(function() {
-
+  var rolls= [];
   $("#player1").click(function() {
 
-    var roll = diceRoller();
+    var roll= diceRoller();
+    rolls.push(roll);
+
+    alert(getSum(rolls));
+    alert(rolls);
+
     $(".die1").text(roll);
     alert(isOne(roll));
 
