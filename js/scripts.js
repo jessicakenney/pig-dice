80fe0d9fb2f7e521196909b92c721d82d9276ca8
Jessica Sheridan and Kheifer Fuller <jessica@>
2017-07-19 11:20:26 -0700

    include constructor and prototypes for player.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,15 @@
-function Player(number, turnTotal, gameTotal){
-  this.number = number;
+function Player (user) {
+  this.number = user;
+  this.rolls = [];
   this.turnTotal = 0;
   this.gameTotal = 0;
+  this.turnOver = 0;
 }
 
-function diceRoller() {
+Player.prototype.diceRoller =function() {
   return Math.floor(Math.random() * 6) + 1;
 }
+
 function getSum(numbers){
   var sum = 0;
   numbers.forEach(function(number) {
@@ -16,28 +19,24 @@ function getSum(numbers){
 }
 
 $(document).ready(function() {
-  var rolls= [];
-  var turnOver = 0;
   var hold = 0;
   $("#roll1").click(function() {
-
-    var roll= diceRoller();
-    rolls.push(roll);
-
+    var player1 = new Player ("1");
+    var roll = player1.diceRoller();
+    player1.rolls.push(roll);
     $(".die1").text(roll);
+
     if(roll=== 1){
-      turnOver = 1;
+      player1.turnOver = 1;
       alert("Turn Over: 1 rolled")
       $("#turnTotal").text("0");
     }else{
-      $("#turnTotal").text(getSum(rolls));
+      $("#turnTotal").text(getSum(player1.rolls));
     }
-
 });
   $("#hold1").click(function() {
     hold = 1;
-    //$("#turnTotal").text(getSum(rolls));
-    //alert("Working!");
+    alert("hold event: turn over")
 });
 
 });
